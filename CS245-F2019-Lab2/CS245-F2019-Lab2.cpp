d6f872f5fa8ee5c24c0722f8659e5c8c697bd229
Nick Chandler <chandlern@thomas.edu>
2019-11-25 19:53:32 -0500

    Went through and cleaned up some simple bugs within the project. Some were having an issue, but I have no idea why.

--- CS245-F2019-Lab2/CS245-F2019-Lab2.cpp
+++ CS245-F2019-Lab2/CS245-F2019-Lab2.cpp
@@ -15,23 +15,31 @@
 
  // Preprocessor directives
 #include <random>
+#include <string>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+
 
 
 int main()
 {
 	// Create a vector of strings; add items using a list initializer
-	vector<String> movies = "Holmes & Watson", "Mega Shark vs. Crocosaurus", "C-SPAN: The Movie", "That's My Boy";
+	vector<string> movies { "Holmes & Watson", "Mega Shark vs. Crocosaurus", "C-SPAN: The Movie", "That's My Boy" };
 
 	// Add a 5th string to the end of the vector
-	movies.add("Titanic 2: The Quickening");
+	movies.push_back("Titanic 2: The Quickening");
 
 	// Announce the nominees
-	cout << "And the nominees for Best Picture (cough) are..." >> endl;
+	std::cout << "And the nominees for Best Picture (cough) are..." << endl;
 
 	// Loop through the vector; print each vector element to console.
-	for (i = 0; i < movies.Length; i++)
+	for (auto i = 0; i < movies.size(); i++)
 	{
-		cout << movies.get(i) << endl;
+		cout << movies.at(i) << endl;
 	}
 
 	// Set up a random number generator seeder
@@ -44,12 +52,12 @@ int main()
 	int pick_a_number = rand_dist(seed);
 
 	// Get the item from the vector at the randomly-selected index
-	String winner = movies.get[pick_a_number];
+	string winner = movies.at(pick_a_number);
 
 	// Convert each charcter of the 'winner' string to UPPERCASE
-	for (char c : winner)
+	for (auto c : winner)
 	{
-		c = c.toUpper();
+		auto b = c.toUpper();
 	}
 
 	// Print the UPPERCASE string
